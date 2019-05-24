1fe10413c3344dccab2961526cec325c495f3d63
kde <kdeun1@naver.com>
2019-05-24 13:47:44 +0900

    restart project

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,88 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div
+    id="app"
+  >
+    <div
+      class="top"
+    >
+      top
     </div>
-    <router-view/>
+    <div
+      class="body"
+    >
+      <div
+        class="navi"
+      >
+        <div>
+          <router-link to="/">
+            main
+          </router-link>
+        </div>
+        <div>
+          <router-link to="/ZeroHour">
+            menu1
+          </router-link>
+        </div>
+      </div>
+      <div
+        class="contents"
+      >
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import 'buefy/dist/buefy.css';
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      checkbox: false,
+    };
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .top {
+    position: absolute;
+    height: 50px;
+    width: 100%;
+    border-bottom: 1px solid #000;
+  }
+  .body {
+    position: absolute;
+    top: 50px;
+    width: 100%;
+    height: calc(100% - 50px);
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .navi {
+    position: absolute;
+    border-right: 1px solid #000;
+    width: 200px;
+    height: 100%;
+    background-color: #FFFFFF;
+  }
+  .contents {
+    width: 100%;
+    height: 100%;
+    margin-left: 200px;
+  }
 </style>
---
+++ src/views/ZeroHour.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-tabs v-model="activeTab">
+      <b-tab-item label="room">
+        <img
+          src="@/assets/zeroHour_room.png"
+        />
+      </b-tab-item>
+      <b-tab-item label="matrix">
+        Lorem ipsum dolor sit amet.
+      </b-tab-item>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZeroHour',
+  components: {
+  },
+  data() {
+    return {
+      activeTab: 0,
+    };
+  },
+};
+</script>
