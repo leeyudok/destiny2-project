c3bc2ee6c416948eeba8da3febbd383ed7bdee55
kde <kdeun1@naver.com>
2019-05-25 14:41:46 +0900

    개시시간 기능 추가 ======================== - 심플모드 추가

--- src/components/ZeroHourSequence.vue
+++ src/components/ZeroHourSequence.vue
@@ -140,7 +140,7 @@ export default {
   data() {
     return {
       defaultSortDirection: 'asc',
-      seqColorFlag: false,
+      seqColorFlag: true,
       simpleModeFlag: false,
       columns: [
         {
